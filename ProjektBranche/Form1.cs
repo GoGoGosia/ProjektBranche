ff62a8da00204cbaa6d6d14cb41e9a5244810e6e
GoGoGosia <malgosiapawlisiak@gmail.com>
2026-04-08 21:40:27 +0200

    First commit

--- ProjektBranche/Form1.cs
+++ ProjektBranche/Form1.cs
@@ -2,9 +2,39 @@ namespace ProjektBranche
 {
     public partial class Form1 : Form
     {
+        Bitmap Image;
+
         public Form1()
         {
             InitializeComponent();
         }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            loadImage();
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loadImage()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Pliki BMP (*.bmp)|*.bmp|Wszystkie pliki (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    Image = new Bitmap(openFileDialog.FileName);
+                    pictureBox1.Image = Image;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("B³¹d podczas wczytywania obrazu: " + ex.Message);
+                }
+            }
+        }
+
     }
 }
